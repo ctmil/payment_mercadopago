acba3f82e349ac54f89739ad23d11f302c919dc8
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2016-06-24 15:08:54 -0300

    FIX response collection external_reference

--- models/mercadopago.py
+++ models/mercadopago.py
@@ -272,9 +272,9 @@ class AcquirerMercadopago(models.Model):
             collection = acq \
                 .mercadopago_get_collection(collection_id)
 
-            import pdb; pdb.set_trace()
+            external_reference = collection.get('collection', {}) \
+                .get('external_reference')
 
-            external_reference = collection.get('external_reference')
             if not external_reference:
                 continue
 
