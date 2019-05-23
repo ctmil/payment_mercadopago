881ea0da5d8b41ca7e72813f380c5d88af20f71e
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-05-23 11:30:39 -0300

    Version 11.0 in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'name': 'MercadoPago Payment Acquirer',
     'category': 'Accounting',
     'summary': 'Payment Acquirer: MercadoPago Implementation',
-    'version': '1.0',
+    'version': '11.0',
     'description': """MercadoPago Payment Acquirer""",
     'author': 'Moldeo Interactive',
     'depends': ['payment'],
