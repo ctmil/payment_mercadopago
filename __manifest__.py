b1c16061f97ec60604c2fb1795a23a11a225cabb
Ignacio Buioli <ibuioli@gmail.com>
2019-10-29 14:03:03 -0300

    feat: add website author

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
     'version': '11.0',
     'description': """MercadoPago Payment Acquirer""",
     'author': 'Moldeo Interactive',
+    'website': 'http://www.moldeointeractive.com.ar',
     'depends': ['payment'],
     'data': [
         'views/mercadopago.xml',
