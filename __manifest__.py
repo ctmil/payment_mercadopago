50356d7093e185e093b1212025f01554f58dbab5
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-10-20 17:19:16 +0200

    Upgrade 31

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Accounting/Payment Acquirers',
     'sequence': 365,
     'summary': 'Payment Acquirer: MercadoPago Implementation',
-    'version': '15.1.30.0',
+    'version': '15.1.31.0',
     'description': """MercadoPago Payment Acquirer""",
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
